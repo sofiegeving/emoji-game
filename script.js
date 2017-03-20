b31276f175061126c27b136fe0f458b83908879e
Sofie Geving <sofie@imail.no>
2017-03-20 19:05:28 +0000

    snap function

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ var currentRound = 0;
 var startNextRound = function () {
 	// round +1 
 	currentRound ++;
-	
+
 	// robot and user to be assigned random emojis
 	robot.currentEmoji = getRandomEmoji();
 	user.currentEmoji = getRandomEmoji();
@@ -34,4 +34,66 @@ var getRandomEmoji = function () {
 
 	return randomEmoji;
 };
+
+var snap = function (robotCalledSnap) {
+	// compare between the two emojis 
+	// if it is the pizza then snap is true 
+	// if snap is true then the user/robot gets +1 to roundsWon 
+	// if user gets it wrong then robot gets +1 to roundsWon 
+	// notification of what happened - console/ui/html 
+
+	var snap = false; 
+	if (robot.currentEmoji == user.currentEmoji) {
+		snap = true; 
+	} else if (robot.currentEmoji == pizza) {
+		snap = true; 
+	} else if (user.currentEmoji == pizza) {
+		snap = true;
+	}
+
+	console.group('Snap called by: '+ (robotCalledSnap ? 'Robot' : 'User'));
+
+	//robot called 
+	if (robotCalledSnap) {
+		if (snap) {
+			robot.roundsWon ++;
+			console.log('robot won the round')
+		} else {
+			user.roundsWon ++;
+			console.log('robot lost the round')
+		}
+	
+	//user called
+	} else {
+		if (snap) {
+			user.roundsWon ++;
+			console.log('you won the round')
+		} else {
+			robot.roundsWon ++;
+			console.log('you lost the round')
+		}
+	}
+
+	console.groupEnd();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
