765757fb627fa92d3cce3a0e1f2b77193f996a59
Sofie Geving <sofie@imail.no>
2017-03-20 18:39:48 +0000

    setup startNextRound function

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('hey good looking!');
+// define players
+var user = {
+	name: 'user',
+	roundsWon: 0, 
+	currentEmoji: undefined
+};
+var robot = {
+	name: 'robot',
+	roundsWon: 0, 
+	currentEmoji: undefined
+};
+
+//define the emojis 
+var emojis = ['🤑','🙈','👑','🎓','🐨','🍑'];
+var pizza = '🍕';
+
+//define state 
+var currentRound = 0; 
+
+var startNextRound = function () {
+	// round +1 
+	currentRound ++;
+	
+	// robot and user to be assigned random emojis
+	robot.currentEmoji = getRandomEmoji();
+	user.currentEmoji = getRandomEmoji();
+
+	console.log('emojis', robot.currentEmoji, 'vs', user.currentEmoji)
+};
+
+var getRandomEmoji = function () {
+	var randomNumber = Math.round(Math.random() * (emojis.length-1));
+	var randomEmoji = emojis[randomNumber]
+
+	return randomEmoji;
+};
+
